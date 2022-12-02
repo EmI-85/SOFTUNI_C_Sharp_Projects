fef9584f5a9ed469bb44cf8c31d8dc10a178016a
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-02 20:19:16 +0200

    Excersizes upload

---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Lab/NumberInRange/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace NumberInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= -100)
+            {
+                if (number <= 100)
+                {
+                    if (number != 0)
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
