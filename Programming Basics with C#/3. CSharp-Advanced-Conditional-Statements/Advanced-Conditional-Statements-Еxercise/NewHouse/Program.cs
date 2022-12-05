d7c61bad45d70cae498802ab7ad9abde1daaa791
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-05 11:05:26 +0200

    Excersizes upload

---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Еxercise/NewHouse/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace NewHouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string flowerType = Console.ReadLine();
+            int flowerNumber = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+
+            double roseStandardPrice = 5.00;
+            double dahliaStandardPrice = 3.80;
+            double tulipStandardPrice = 2.80;
+            double narcissStandardPrice = 3.00;
+            double gladiolusStandardPrice = 2.50;
+            double flowersCost = 0;
+
+            switch(flowerType)
+            {
+                case "Roses":
+                    if(flowerNumber > 80)
+                    {
+                    flowersCost = flowerNumber * roseStandardPrice * 0.9;
+                    }
+                    else
+                    {
+                    flowersCost = flowerNumber * roseStandardPrice;
+                    }
+                    break;
+                case "Dahlias":
+                    if(flowerNumber > 90)
+                    {
+                        flowersCost = flowerNumber * dahliaStandardPrice * 0.85;
+                    }
+                    else
+                    {
+                        flowersCost = flowerNumber * dahliaStandardPrice;
+                    }
+                    break;
+                case "Tulips":
+                    if (flowerNumber > 80)
+                    {
+                        flowersCost = flowerNumber * tulipStandardPrice * 0.85;
+                    }
+                    else
+                    {
+                        flowersCost = flowerNumber * tulipStandardPrice;
+                    }
+                    break;
+                case "Narcissus":
+                    if (flowerNumber < 120)
+                    {
+                        flowersCost = flowerNumber * narcissStandardPrice * 1.15;
+                    }
+                    else
+                    {
+                        flowersCost = flowerNumber * narcissStandardPrice;
+                    }
+                    break;
+                case "Gladiolus":
+                    if (flowerNumber < 80)
+                    {
+                        flowersCost = flowerNumber * gladiolusStandardPrice * 1.20;
+                    }
+                    else
+                    {
+                        flowersCost = flowerNumber * gladiolusStandardPrice;
+                    }
+                    break;
+            }
+
+            if(budget >= flowersCost)
+            {
+                double moneyLeft = budget - flowersCost;
+                Console.WriteLine($"Hey, you have a great garden with {flowerNumber} {flowerType} and {moneyLeft:f2} leva left.");
+            }
+            else
+            {
+                double moneyNeeded = flowersCost - budget;
+                Console.WriteLine($"Not enough money, you need {moneyNeeded:f2} leva more.");
+            }
+           
+        }
+    }
+}
---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Еxercise/OnTimeForTheExam/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace OnTimeForTheExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int examHour = int.Parse(Console.ReadLine());
+            int examMin = int.Parse(Console.ReadLine());
+            int arrivalHour = int.Parse(Console.ReadLine());
+            int arrivalMin = int.Parse(Console.ReadLine());
+
+            int examTimeAsMinutes = examHour * 60 + examMin;
+            int arrivalTimeAsMinutes = arrivalHour * 60 + arrivalMin;
+
+            int differenceExamArrival = examTimeAsMinutes - arrivalTimeAsMinutes;
+            int differenceHours = differenceExamArrival / 60;
+            int differenceMinutes = differenceExamArrival % 60;
+
+            if (differenceExamArrival == 0)
+            {
+                Console.WriteLine("On time");
+            }
+            else if (differenceExamArrival > 0)
+            {
+                if(differenceExamArrival <= 30)
+                {
+                    Console.WriteLine("On time");
+                    Console.WriteLine($"{differenceMinutes} minutes before the start");
+                }
+                else 
+                {
+                    Console.WriteLine("Early");
+                    if(differenceHours <= 0)
+                    {
+                        Console.WriteLine($"{differenceMinutes} minutes before the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{differenceHours}:{differenceMinutes:d2} hours before the start");
+                    }
+                }
+            }
+            else if (differenceExamArrival < 0)
+            {
+                int differenceExamArrivalLate = arrivalTimeAsMinutes - examTimeAsMinutes;
+                int differenceHoursLate = Math.Abs(differenceExamArrival / 60);
+                int differenceMinutesLate = Math.Abs(differenceExamArrival % 60);
+                
+                Console.WriteLine("Late");
+
+                    if (differenceHoursLate <= 0)
+                    {
+                        Console.WriteLine($"{differenceMinutesLate} minutes after the start");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{differenceHoursLate}:{differenceMinutesLate:d2} hours after the start");
+                    }
+            }
+
+        }
+    }
+}
