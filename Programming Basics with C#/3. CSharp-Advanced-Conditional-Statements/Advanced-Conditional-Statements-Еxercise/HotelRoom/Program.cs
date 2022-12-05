e3691cc1f12b9f177d46917c722e33199dce1155
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-05 11:04:44 +0200

    Excersizes upload

---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Еxercise/HotelRoom/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace HotelRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int numberOfNights = int.Parse(Console.ReadLine());
+            double studioPrice = 0;
+            double apartmentPrice = 0;
+
+            if (month == "May" || month == "October")
+            {
+                if (numberOfNights > 7 && numberOfNights <= 14)
+                {
+                    studioPrice = numberOfNights * 50 * 0.95;
+                    apartmentPrice = numberOfNights * 65;
+                }
+                else if (numberOfNights > 14)
+                {
+                    studioPrice = numberOfNights * 50 * 0.70;
+                    apartmentPrice = numberOfNights * 65 * 0.90;
+                }
+                else
+                {
+                    studioPrice = numberOfNights * 50;
+                    apartmentPrice = numberOfNights * 65;
+                }
+                Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+                Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+            }
+
+            if (month == "June" || month == "September")
+            {
+                if (numberOfNights > 14)
+                {
+                    studioPrice = numberOfNights * 75.20 * 0.80;
+                    apartmentPrice = numberOfNights * 68.70 * 0.90;
+                }
+                else
+                {
+                    studioPrice = numberOfNights * 75.20;
+                    apartmentPrice = numberOfNights * 68.70;
+                }
+                Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+                Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+            }
+
+            if (month == "July" || month == "August")
+            {
+                if (numberOfNights > 14)
+                {
+                    studioPrice = numberOfNights * 76;
+                    apartmentPrice = numberOfNights * 77 * 0.90;
+                }
+                else
+                {
+                    studioPrice = numberOfNights * 76;
+                    apartmentPrice = numberOfNights * 77;
+                }
+                Console.WriteLine($"Apartment: {apartmentPrice:f2} lv.");
+                Console.WriteLine($"Studio: {studioPrice:f2} lv.");
+            }
+        }
+    }
+}
+
+
---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Еxercise/Journey/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Journey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            string accomodationType = "";
+            string destination = "";
+            double tripPrice = 0;
+
+            if (budget <= 100)
+            {
+                destination = "Bulgaria";
+                if(season == "summer")
+                {
+                    accomodationType = "Camp";
+                    tripPrice = 0.30 * budget;
+                    Console.WriteLine($"Somewhere in {destination}");
+                    Console.WriteLine($"{accomodationType} - {tripPrice:f2}");
+                }
+                else if (season == "winter")
+                {
+                    accomodationType = "Hotel";
+                    tripPrice = 0.70 * budget;
+                    Console.WriteLine($"Somewhere in {destination}");
+                    Console.WriteLine($"{accomodationType} - {tripPrice:f2}");
+                }
+            }
+            else if(budget > 100 && budget <= 1000)
+            {
+                destination = "Balkans";
+                if (season == "summer")
+                {
+                    accomodationType = "Camp";
+                    tripPrice = 0.40 * budget;
+                    Console.WriteLine($"Somewhere in {destination}");
+                    Console.WriteLine($"{accomodationType} - {tripPrice:f2}");
+                }
+                else if (season == "winter")
+                {
+                    accomodationType = "Hotel";
+                    tripPrice = 0.80 * budget;
+                    Console.WriteLine($"Somewhere in {destination}");
+                    Console.WriteLine($"{accomodationType} - {tripPrice:f2}");
+                }
+            }
+            else if(budget > 1000)
+            {
+                destination = "Europe";
+                accomodationType = "Hotel";
+                tripPrice = 0.90 * budget;
+                Console.WriteLine($"Somewhere in {destination}");
+                Console.WriteLine($"{accomodationType} - {tripPrice:f2}");
+            }
+        }
+    }
+}
