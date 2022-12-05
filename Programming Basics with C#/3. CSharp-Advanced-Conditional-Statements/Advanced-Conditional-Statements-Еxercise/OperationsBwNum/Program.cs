1431726ddfc6370390965786d143acdaa6f1aaec
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-05 11:07:08 +0200

    Excersizes upload

---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Еxercise/OperationsBwNum/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+            double result = 0;
+            string finalResult = null;
+
+            switch (symbol)
+            {
+                case'+':
+                    result = number1 + number2;
+                    if(result % 2 == 0)
+                    {
+                        finalResult = $"{number1} {symbol} {number2} = {result} - even";
+                    }
+                    else
+                    {
+                        finalResult = $"{number1} {symbol} {number2} = {result} - odd";
+                    }
+                    break;
+                case '-':
+                    result = number1 - number2;
+                    if (result % 2 == 0)
+                    {
+                        finalResult = $"{number1} {symbol} {number2} = {result} - even";
+                    }
+                    else
+                    {
+                        finalResult = $"{number1} {symbol} {number2} = {result} - odd";
+                    }
+                    break;
+                case '*':
+                    result = number1 * number2;
+                    if (result % 2 == 0)
+                    {
+                        finalResult = $"{number1} {symbol} {number2} = {result} - even";
+                    }
+                    else
+                    {
+                        finalResult = $"{number1} {symbol} {number2} = {result} - odd";
+                    }
+                    break;
+                case '/':
+                    if(number2 == 0)
+                    {
+                        finalResult = $"Cannot divide {number1} by zero";
+                    }
+                    else
+                    {
+                        result = number1 / (double)number2;
+                        finalResult = $"{number1} {symbol} {number2} = {result:f2}";
+                    }
+                    break;
+                case '%':
+                    if (number2 == 0)
+                    {
+                        finalResult = $"Cannot divide {number1} by zero";
+                    }
+                    else
+                    {
+                        result = number1 % number2;
+                        finalResult = $"{number1} {symbol} {number2} = {result}";
+                    }
+                    break;
+            }
+            Console.WriteLine(finalResult);
+
+        }
+    }
+}
---
+++ Programming Basics with C#/3. CSharp-Advanced-Conditional-Statements/Advanced-Conditional-Statements-Еxercise/SkiTrip/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace SkiTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysStay = int.Parse(Console.ReadLine());
+            string typeOfRoom = Console.ReadLine();
+            string feedback = Console.ReadLine();
+
+            int nightsStay = daysStay - 1;
+            double roomForOnePersonPrice = 18;
+            double apartmentPrice = 25;
+            double presidentApartmentPrice = 35;
+            double priceForStay = 0;
+
+            if(typeOfRoom == "room for one person")
+            {
+                if(feedback == "positive")
+                {
+                priceForStay = nightsStay * roomForOnePersonPrice * 1.25;
+                Console.WriteLine($"{priceForStay:f2}");
+                }
+                else if (feedback == "negative")
+                {
+                    priceForStay = nightsStay * roomForOnePersonPrice * 0.90;
+                    Console.WriteLine($"{priceForStay:f2}");
+                }
+            }
+            else if(typeOfRoom == "apartment")
+            {
+                if (feedback == "positive")
+                {
+                    if(daysStay < 10)
+                    {
+                        priceForStay = (nightsStay * apartmentPrice * 0.70) * 1.25;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay >= 10 && daysStay <= 15)
+                    {
+                        priceForStay = (nightsStay * apartmentPrice * 0.65) * 1.25;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay > 15)
+                    {
+                        priceForStay = (nightsStay * apartmentPrice * 0.50) * 1.25;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                }
+                else if (feedback == "negative")
+                {
+                    if (daysStay < 10)
+                    {
+                        priceForStay = (nightsStay * apartmentPrice * 0.70) * 0.9;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay >= 10 && daysStay <= 15)
+                    {
+                        priceForStay = (nightsStay * apartmentPrice * 0.65) * 0.9;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay > 15)
+                    {
+                        priceForStay = (nightsStay * apartmentPrice * 0.50) * 0.9;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                }
+
+            }
+            else if(typeOfRoom == "president apartment")
+            {
+                if (feedback == "positive")
+                {
+                    if (daysStay < 10)
+                    {
+                        priceForStay = (nightsStay * presidentApartmentPrice * 0.90) * 1.25;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay >= 10 && daysStay <= 15)
+                    {
+                        priceForStay = (nightsStay * presidentApartmentPrice * 0.85) * 1.25;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay > 15)
+                    {
+                        priceForStay = (nightsStay * presidentApartmentPrice * 0.80) * 1.25;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                }
+                else if (feedback == "negative")
+                {
+                    if (daysStay < 10)
+                    {
+                        priceForStay = (nightsStay * presidentApartmentPrice * 0.90) * 0.9;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay >= 10 && daysStay <= 15)
+                    {
+                        priceForStay = (nightsStay * presidentApartmentPrice * 0.85) * 0.9;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                    else if (daysStay > 15)
+                    {
+                        priceForStay = (nightsStay * presidentApartmentPrice * 0.80) * 0.9;
+                        Console.WriteLine($"{priceForStay:f2}");
+                    }
+                }
+            }
+        }
+    }
+}
