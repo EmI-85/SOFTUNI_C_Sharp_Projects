f79d2272ebac75b8a9351fd44934fb328b3c6284
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-06 17:49:12 +0200

    Excersizes upload

---
+++ Programming Basics with C#/4. CSharp-For-Loop/For-Loop-Lab/OddEvenSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumEven = 0;
+            int sumOdd = 0;
+
+            for(int i = 1; i < n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    int currentNumber = int.Parse(Console.ReadLine());
+                    sumEven += currentNumber;
+                }
+                else
+                {
+                    int currentNumber = int.Parse(Console.ReadLine());
+                    sumOdd += currentNumber;
+                }
+            }
+
+            if(sumEven == sumOdd)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {sumEven}");
+            }
+            else 
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(sumEven-sumOdd)}");
+            }
+        }
+    }
+}
