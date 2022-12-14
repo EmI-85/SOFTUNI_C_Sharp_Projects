ef2f6a426dc03a7b7f4b0bb5b80ccb0a9b7358e4
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-14 19:35:56 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/CSharp-While-Loop-Lab/AccountBalance/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double balance = 0;
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+                
+                if (input == "NoMoreMoney")
+                {
+                    break;
+                }
+
+                double money = double.Parse(input);
+
+                if (money < 0)
+                {
+                    Console.WriteLine($"Invalid operation!");
+                    break;
+                }
+                
+                balance += money;
+                Console.WriteLine($"Increase: {money:f2}");
+            }
+
+            Console.WriteLine($"Total: {balance:f2}");
+
+        }
+    }
+}
