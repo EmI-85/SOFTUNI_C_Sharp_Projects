1482752933922d72c543c2f162e922b89d879787
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-14 19:36:35 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/CSharp-While-Loop-Lab/MaxNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue;
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "Stop")
+                {
+                    break;
+                }
+
+                int currentNumber = int.Parse(input);
+                
+                if(currentNumber > maxNumber)
+                {
+                    maxNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(maxNumber);
+
+        }
+    }
+}
