7b7dd29ebba1269faef1627aec557ba85c89c889
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-14 19:36:17 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/CSharp-While-Loop-Lab/Graduation/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            double grades = 0;
+            bool hasFailed = false;
+            bool hasGraduated = true;
+            int year = 1;
+
+            while(year <= 12)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                
+                if(grade < 4)
+                {
+                    if(hasFailed)
+                    {
+                        Console.WriteLine($"{studentName} has been excluded at {year} grade");
+                        hasGraduated = false;
+                        break;
+                    }
+
+                    hasFailed = true;
+                    continue;   
+                }
+                
+                grades += grade;
+                year++;
+            }
+
+            if(hasGraduated)
+            { 
+                Console.WriteLine($"{studentName} graduated. Average grade: {(grades / 12):f2}");
+            }
+
+        }
+    }
+}
