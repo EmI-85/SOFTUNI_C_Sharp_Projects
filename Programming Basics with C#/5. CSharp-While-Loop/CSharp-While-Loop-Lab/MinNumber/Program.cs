d86b9f08db02d4c494e7cfbf3688fca57012f6e8
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-14 19:37:00 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/CSharp-While-Loop-Lab/MinNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Stop")
+                {
+                    break;
+                }
+
+                int currentNumber = int.Parse(input);
+
+                if (currentNumber < minNumber)
+                {
+                    minNumber = currentNumber;
+                }
+            }
+
+            Console.WriteLine(minNumber);
+
+        }
+    }
+}
