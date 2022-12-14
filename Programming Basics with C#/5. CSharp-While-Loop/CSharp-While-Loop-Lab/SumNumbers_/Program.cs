2ae85cc95c393d5c47b4afe42a03014245e5f5e5
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-14 19:38:39 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/CSharp-While-Loop-Lab/SumNumbers_/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumNumbers_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while(sum < n)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
