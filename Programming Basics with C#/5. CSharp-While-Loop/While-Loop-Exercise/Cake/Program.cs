65258f0f19c6c37f4ca5c617fef12e26363603f4
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-15 20:33:41 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/While-Loop-Exercise/Cake/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int cakeArea = width * length;
+            string command = "";
+
+            bool cakeIsFinished = false;
+
+            while(true)
+            {
+                command = Console.ReadLine();
+
+                if(command == "STOP")
+                {
+                    break;
+                }
+
+               int pieces = int.Parse(command);
+
+                cakeArea -= pieces;
+
+                if (cakeArea <= 0)
+                {
+                    cakeIsFinished = true;
+                    break;
+                }
+            }
+
+            if(cakeIsFinished)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs(cakeArea)} pieces more.");
+            }
+            else
+            {
+                Console.WriteLine($"{cakeArea} pieces are left.");
+            }
+
+        }
+    }
+}
