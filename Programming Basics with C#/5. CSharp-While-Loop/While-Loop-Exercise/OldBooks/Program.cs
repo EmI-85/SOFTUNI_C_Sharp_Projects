59451e0eca178b644ce5df27607ae0f4b9634d55
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-15 20:35:35 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/While-Loop-Exercise/OldBooks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            int bookNum = 0;
+            bool isBookFound = false;
+
+            string selectedBookName = Console.ReadLine();
+
+            while (selectedBookName != "No More Books!")
+            {
+                selectedBookName = Console.ReadLine();
+
+                if (selectedBookName == favouriteBook)
+                {
+                    isBookFound = true;
+                    break;
+                }
+                
+                bookNum++;
+                
+            }
+
+            if (isBookFound)
+            {
+                Console.WriteLine($"You checked {bookNum} books and found it.");
+            }
+            else
+            {
+                    Console.WriteLine($"The book you search is not here!");
+                    Console.WriteLine($"You checked {bookNum} books.");
+            }
+
+        }
+    }
+}
