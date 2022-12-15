ddcaa9723bcb9dec396f94a99335baa8e369336b
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-15 20:35:00 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/While-Loop-Exercise/ExamPreparation/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ExamPreparation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPoorGrades = int.Parse(Console.ReadLine());
+
+            int poorGradesCounter = 0;
+            int numberOfProblems = 0;
+            int sumOfGrades = 0;
+            string lastProblemName = "";
+            bool hasFailed = true;
+
+            while (poorGradesCounter < numberOfPoorGrades)
+            {
+                string problemName = Console.ReadLine();
+                
+                if(problemName == "Enough")
+                {
+                    hasFailed = false;
+                    break;
+                }
+
+                int grade = int.Parse(Console.ReadLine());
+
+                if(grade <= 4)
+                {
+                    poorGradesCounter++;
+                }
+
+                sumOfGrades += grade;
+                numberOfProblems++;
+                lastProblemName = problemName;
+            }
+
+            if(hasFailed)
+            {
+                Console.WriteLine($"You need a break, {poorGradesCounter} poor grades.");
+            }
+            else
+            {
+            Console.WriteLine($"Average score: {(sumOfGrades / (double)numberOfProblems):f2}");
+            Console.WriteLine($"Number of problems: {numberOfProblems}");
+            Console.WriteLine($"Last problem: {lastProblemName}");
+            }
+        }
+    }
+}
