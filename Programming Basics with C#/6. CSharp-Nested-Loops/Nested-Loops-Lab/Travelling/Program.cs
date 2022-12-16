a6609a0a820b426dc1d5bfc4a7cea6d843139a3e
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-16 12:22:30 +0200

    Excersizes upload

---
+++ Programming Basics with C#/6. CSharp-Nested-Loops/Nested-Loops-Lab/Travelling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = "";
+
+            while((destination = Console.ReadLine()) != "End" )
+            {
+                double destinationPrice = double.Parse(Console.ReadLine());
+
+                double savedMoney = 0;
+
+                while(destinationPrice > savedMoney)
+                {
+                    savedMoney += double.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+            }
+
+
+        }
+    }
+}
