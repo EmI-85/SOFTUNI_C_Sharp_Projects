67abe09f3db7ef3255aaef40b7f024ebb210bcd1
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-16 12:21:56 +0200

    Excersizes upload

---
+++ Programming Basics with C#/6. CSharp-Nested-Loops/Nested-Loops-Lab/SumOfTwoNums/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lowerBound = int.Parse(Console.ReadLine());
+            int upperBound = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int combination = 0;
+            bool isMagicNumberFound = false;
+
+            for(int i = lowerBound; i <= upperBound; i++)
+            {
+                for (int j = lowerBound; j <= upperBound; j++)
+                {
+                    combination++;
+
+                    if(i + j == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{combination} ({i} + {j} = {magicNumber})");
+                        isMagicNumberFound = true;
+                        break;
+                    } 
+                }
+
+                if (isMagicNumberFound)
+                {
+                    break;
+                }
+            }
+
+            if(!isMagicNumberFound)
+            {
+                Console.WriteLine($"{combination} combinations - neither equals {magicNumber}");
+            }
+            
+
+        }
+    }
+}
