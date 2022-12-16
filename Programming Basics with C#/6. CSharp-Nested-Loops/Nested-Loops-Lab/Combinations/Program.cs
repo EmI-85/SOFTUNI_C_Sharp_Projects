1d30fe484d52ec65baac2488d8116a3859f5877c
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-16 12:21:21 +0200

    Excersizes upload

---
+++ Programming Basics with C#/6. CSharp-Nested-Loops/Nested-Loops-Lab/Combinations/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Combinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int validCombinations = 0;
+
+            for(int x = 0; x <= number; x++)
+            {
+
+                for (int y = 0; y <= number; y++)
+                {
+                    for (int z = 0; z <= number; z++)
+                    {
+                        if(x + y + z == number)
+                        {
+                            validCombinations++;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(validCombinations);
+        }
+    }
+}
