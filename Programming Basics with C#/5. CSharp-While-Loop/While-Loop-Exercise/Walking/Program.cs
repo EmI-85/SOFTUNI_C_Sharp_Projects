bf404dcd37298c0e952044a79e74a12fd0617e54
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-16 12:20:32 +0200

    Excersizes upload

---
+++ Programming Basics with C#/5. CSharp-While-Loop/While-Loop-Exercise/Walking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dailyStepsGoal = 10000;
+            int totalSteps = 0;
+            string command = "";
+
+            while(totalSteps < dailyStepsGoal)
+            {
+                command = Console.ReadLine();
+
+                if(command == "Going home")
+                {
+                    int lastSteps = int.Parse(Console.ReadLine());
+                    totalSteps += lastSteps;
+                    break;
+                }
+
+                int steps = int.Parse(command);
+                totalSteps += steps;
+            }
+
+            if(totalSteps >= dailyStepsGoal)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+
+                int stepsMoreThanGoal = totalSteps - dailyStepsGoal;
+                Console.WriteLine($"{stepsMoreThanGoal} steps over the goal!");
+            }
+            else
+            {
+                int stepsLess = dailyStepsGoal - totalSteps;
+                Console.WriteLine($"{stepsLess} more steps to reach goal.");
+            }
+
+        }
+
+    }
+}
