ad7c2f748a621f7a9f2da5e2f9199629b4bcc226
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-19 17:07:04 +0200

    Excersizes upload

---
+++ Programming Basics with C#/6. CSharp-Nested-Loops/Nested-Loops-Exercise/TrainTheTrainers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryMembersNum = int.Parse(Console.ReadLine());
+
+            double totalAverageGrades = 0;
+            int presentationsCounter = 0;
+            
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "Finish")
+                {
+                    break;
+                }
+
+                string presentationTitle = input;
+                double gradesForPresentation = 0;
+
+                presentationsCounter++;
+
+                for (int i = 1; i <= juryMembersNum; i++)
+                {
+                    double grade = double.Parse(Console.ReadLine());
+                    gradesForPresentation += grade;
+                }
+
+                double averageGradeFromPresentation = gradesForPresentation / juryMembersNum;
+                Console.WriteLine($"{presentationTitle} - {averageGradeFromPresentation:f2}.");
+
+                totalAverageGrades += averageGradeFromPresentation;
+            }
+
+            double averageFromCourse = totalAverageGrades / presentationsCounter;
+            Console.WriteLine($"Student's final assessment is {averageFromCourse:f2}.");
+
+        }
+    }
+}
