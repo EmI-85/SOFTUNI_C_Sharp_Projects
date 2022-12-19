026a5750b503da37e8252e94d66f329c6f551956
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-19 17:06:47 +0200

    Excersizes upload

---
+++ Programming Basics with C#/6. CSharp-Nested-Loops/Nested-Loops-Exercise/SumPrimeNonPrime/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace SumPrimeNonPrime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            int primesSum = 0;
+            int compoundsSum = 0;
+
+            while (command != "stop")
+            {
+                command = Console.ReadLine();
+                if(command == "stop")
+                {
+                    break;
+                }
+
+                int num = int.Parse(command);
+                if(num < 0)
+                {
+                    Console.WriteLine("Number is negative.");
+                    continue;
+                }
+
+                bool isPrime = true;
+
+                for(int divider = 2; divider < num; divider++)
+                {
+                    int remainder = num % divider;
+
+                    if(remainder == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primesSum += num;
+                }
+                else
+                {
+                    compoundsSum += num;
+                }
+            }
+
+            Console.WriteLine($"Sum of all prime numbers is: {primesSum}");
+            Console.WriteLine($"Sum of all non prime numbers is: {compoundsSum}");
+
+        }
+    }
+}
