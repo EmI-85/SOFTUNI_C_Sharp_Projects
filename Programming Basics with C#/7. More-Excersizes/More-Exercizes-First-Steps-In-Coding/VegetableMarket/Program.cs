9878f8167ef1058b900399050d90a01cdc8169b2
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-21 12:58:55 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-First-Steps-In-Coding/VegetableMarket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BglEurRate = 1.94;
+            double vegetablePrice = double.Parse(Console.ReadLine());
+            double fruitPrice = double.Parse(Console.ReadLine());
+            int vegetablesKg = int.Parse(Console.ReadLine());
+            int fruitsKg = int.Parse(Console.ReadLine());
+
+            double totalPriceinBgl = vegetablePrice * vegetablesKg + fruitPrice*fruitsKg;
+            double totalPriceinEur = totalPriceinBgl / BglEurRate;
+
+            Console.WriteLine("{0:0.00}", totalPriceinEur);
+        }
+    }
+}
