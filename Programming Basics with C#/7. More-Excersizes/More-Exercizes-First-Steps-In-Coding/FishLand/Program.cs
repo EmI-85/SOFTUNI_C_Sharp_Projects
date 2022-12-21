9857d8240231696cd03c8a1e5ce03a32b55374a9
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-21 12:57:28 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-First-Steps-In-Coding/FishLand/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FishLand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skumriaPricePerKg = double.Parse(Console.ReadLine());
+            double cacaPricePerKg = double.Parse(Console.ReadLine());
+            double palamudKilos = double.Parse(Console.ReadLine());
+            double safridKilos = double.Parse(Console.ReadLine());
+            double midiKilos = double.Parse(Console.ReadLine());
+
+            double midiPricePerKg = 7.50;
+            double palamudPricePerKg = skumriaPricePerKg * 1.6;
+            double safridPricePerKg = cacaPricePerKg * 1.8;
+
+           double bill = palamudKilos*palamudPricePerKg + safridKilos*safridPricePerKg + midiKilos*midiPricePerKg;
+
+            Console.WriteLine($"{bill:f2}");
+
+        }
+    }
+}
