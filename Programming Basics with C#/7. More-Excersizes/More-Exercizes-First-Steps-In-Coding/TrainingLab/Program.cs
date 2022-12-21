9a37327057aa674d4898bd312c36ecb8208dbb4f
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-21 12:58:06 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-First-Steps-In-Coding/TrainingLab/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double trainingLabWidthWithoutCorridor = h - 1;
+
+            double deskSize = 0.70;
+            double deskSizeWidth = 1.20;
+            double numberOfDesksPerRow = Math.Floor(trainingLabWidthWithoutCorridor/deskSize);
+            double numberOfRows = Math.Floor(w/deskSizeWidth);
+            double numberOfSeats = numberOfDesksPerRow * numberOfRows - 3;
+
+            Console.WriteLine(numberOfSeats);
+        }
+    }
+}
