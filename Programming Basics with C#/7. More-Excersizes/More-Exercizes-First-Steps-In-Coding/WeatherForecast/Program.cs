3c4e4276cf7f25616614ca10cd92259f8c568868
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-21 12:59:10 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-First-Steps-In-Coding/WeatherForecast/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WeatherForecast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string weatherType = Console.ReadLine();
+
+            if(weatherType == "sunny")
+            {
+                Console.WriteLine("It's warm outside!");
+            }
+            else
+            {
+                Console.WriteLine("It's cold outside!");
+            }
+
+
+
+        }
+    }
+}
