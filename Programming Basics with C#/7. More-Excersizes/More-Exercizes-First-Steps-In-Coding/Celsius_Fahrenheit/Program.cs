7ff2a0200f297a520fbedf9a6b0df9abbcac8f8f
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-21 12:52:41 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-First-Steps-In-Coding/Celsius_Fahrenheit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Celsius_to_Fahrenheit_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degreesCelsius = double.Parse(Console.ReadLine());
+            double degreesFahrenheit = degreesCelsius * 1.8 + 32;
+
+            Console.WriteLine($"{degreesFahrenheit:f2}");
+
+        }
+    }
+}
