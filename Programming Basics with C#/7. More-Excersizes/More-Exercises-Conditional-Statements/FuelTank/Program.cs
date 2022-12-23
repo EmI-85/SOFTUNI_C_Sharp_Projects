376d668242edaf2130de10ed842d8100da15b8e9
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-23 16:56:13 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercises-Conditional-Statements/FuelTank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FuelTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine();
+            double litersFuelInTank = double.Parse(Console.ReadLine());
+
+            if (fuelType == "Diesel" || fuelType == "Gasoline" || fuelType == "Gas")
+            {
+                if (litersFuelInTank >= 25)
+                {
+                    fuelType = fuelType.ToLower();
+                    Console.WriteLine($"You have enough {fuelType}.");
+                }
+                else if (litersFuelInTank < 25)
+                {
+                    fuelType = fuelType.ToLower();
+                    Console.WriteLine($"Fill your tank with {fuelType}!");
+                }
+            }
+            else
+                {
+                Console.WriteLine($"Invalid fuel!"); 
+                }
+                    
+            }
+
+        }
+    }
+
