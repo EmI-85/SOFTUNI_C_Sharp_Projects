20938dd17d497092758bb20dabe95e6366df2133
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-23 16:57:28 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercises-Conditional-Statements/Pets/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfDays = int.Parse(Console.ReadLine());
+            int foodLeftByOwner = int.Parse(Console.ReadLine());
+            double dogFoodPerDay = double.Parse(Console.ReadLine());
+            double catFoodPerDay = double.Parse(Console.ReadLine());
+            double turtleFoodPerDay = double.Parse(Console.ReadLine());
+
+            double dogFoodNeeded = dogFoodPerDay * numberOfDays;
+            double catFoodNeeded = catFoodPerDay * numberOfDays;
+            double turtleFoodNeeded = turtleFoodPerDay / 1000 * numberOfDays;
+            double totalFoodNeeded = dogFoodNeeded + catFoodNeeded + turtleFoodNeeded;
+
+            if (totalFoodNeeded <= foodLeftByOwner)
+            {
+                double foodDifferenceInKilos = Math.Floor(foodLeftByOwner - totalFoodNeeded);
+                Console.WriteLine($"{foodDifferenceInKilos} kilos of food left.");
+            }
+            else
+            {
+                double foodDifferenceInKilos = Math.Ceiling(totalFoodNeeded - foodLeftByOwner);
+                Console.WriteLine($"{foodDifferenceInKilos} more kilos of food are needed.");
+            }
+
+        }
+    }
+}
