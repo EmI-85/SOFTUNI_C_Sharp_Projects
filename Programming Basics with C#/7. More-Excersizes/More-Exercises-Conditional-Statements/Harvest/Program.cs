c2b1c5f343ed2029924620731325fc27bda4634a
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-23 16:56:48 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercises-Conditional-Statements/Harvest/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Harvest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            double y = double.Parse(Console.ReadLine());
+            int z = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double harvest = x * y;
+            double wineProducingPortion = 0.40 * harvest;
+            double wineLitersProduced = wineProducingPortion / 2.5;
+
+            double difference = Math.Abs(wineLitersProduced - z);
+
+            if(wineLitersProduced < z)
+            {
+                difference = Math.Floor(difference);
+             Console.WriteLine($"It will be a tough winter! More {difference} liters wine needed.");
+            }
+            else
+            {
+                wineLitersProduced = Math.Floor(wineLitersProduced);
+                Console.WriteLine($"Good harvest this year! Total wine: {wineLitersProduced} liters.");
+
+                double wineLeft = Math.Ceiling(wineLitersProduced -z);
+                double winePerWorker = Math.Ceiling(wineLeft / workers);
+                Console.WriteLine($"{wineLeft} liters left -> {winePerWorker} liters per person.");
+            }
+
+        }
+    }
+}
