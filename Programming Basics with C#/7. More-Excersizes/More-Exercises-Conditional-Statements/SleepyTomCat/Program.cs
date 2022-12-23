47f11aa5192fc32937e15a84aeb97f8ae5a1f84c
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-23 16:58:04 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercises-Conditional-Statements/SleepyTomCat/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SleepyTomCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeDays = int.Parse(Console.ReadLine());
+            int workingDays = 365 - freeDays;
+            int yearlyNorm = 30000;
+            int workingDayPlayPerDay = 63;
+            int freeDayPlayPerDay = 127;
+
+            double actualPlay = freeDays * freeDayPlayPerDay + workingDayPlayPerDay * workingDays;
+            double timeOverNorm = Math.Abs(actualPlay - yearlyNorm);
+           
+            if (actualPlay > yearlyNorm)
+            {
+                double H = Math.Floor(timeOverNorm / 60);
+                double M = timeOverNorm % 60;
+
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{H} hours and {M} minutes more for play");
+            }
+            else if(actualPlay <= yearlyNorm)
+            {
+                double H = Math.Floor(timeOverNorm / 60);
+                double M = timeOverNorm % 60;
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{H} hours and {M} minutes less for play");
+            }
+
+        }
+    }
+}
