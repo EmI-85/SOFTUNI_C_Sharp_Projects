e94c9e2386e77eac5eea14e837a1d2763e887ded
Emiliya Kirina <emiliya.kirina@gmail.com>
2022-12-23 16:58:30 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercises-Conditional-Statements/TransportPrice/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TransportPrice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string timeType = Console.ReadLine();
+
+            double taxiPriceDay = 0.70 + 0.79 * n;
+            double taxiPriceNight = 0.70 + 0.90 * n;
+            double busPriceDayorNight = 0.09 * n; // min 20km
+            double trainPriceDayorNight = 0.06 * n; // min 100 km
+
+            if (n < 20)
+            {
+                if (timeType == "day")
+                {
+                    Console.WriteLine($"{taxiPriceDay:f}");
+                }
+                else
+                {
+                    Console.WriteLine($"{taxiPriceNight:f}");
+                }
+            }
+
+            else if (n >= 20 && n < 100)
+            {
+                Console.WriteLine($"{busPriceDayorNight:f}");
+            }
+
+            else if (n >= 100)
+            {
+                Console.WriteLine($"{trainPriceDayorNight:f}");
+            }
+
+        }
+    }
+}
