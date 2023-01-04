0ef51908e633287a05c0877eedb853118fadd5b2
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-04 18:06:22 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-For-Loop/Hospital/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+            
+
+            int numberOfDoctors = 7;
+            int treatedPatients = 0;
+            int unTreatedPatients = 0;
+            
+
+           for (int i = 1; i <= period; i++)
+                {
+                int numberOfIncomingPatients = int.Parse(Console.ReadLine());
+
+                if (i % 3 == 0 && treatedPatients < unTreatedPatients)
+                {
+                    numberOfDoctors++;
+                }
+
+                if (numberOfIncomingPatients > numberOfDoctors)
+                    {
+                    unTreatedPatients += (numberOfIncomingPatients - numberOfDoctors);
+                    treatedPatients += numberOfDoctors;
+                    }
+                    else
+                    {
+                    treatedPatients += numberOfIncomingPatients;
+                    }
+           }
+              
+            Console.WriteLine($"Treated patients: {treatedPatients}.");
+            Console.WriteLine($"Untreated patients: {unTreatedPatients}.");
+        }
+    }
+}
+
