a19161989313f371ce251ed1a6f8e41857ef7f81
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-09 11:50:29 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-While-Loop/AverageNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            double numSum = 0;
+            int counter = 0;
+
+            for(int i = 1; i <= n ; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                numSum += currentNum;
+                counter++;
+            }
+
+            double average = numSum / counter;
+
+            Console.WriteLine($"{average:f2}");
+        }
+    }
+}
