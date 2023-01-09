afb282e30171939697a7f017774f802261f222db
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-09 11:51:35 +0200

    Excersizes upload

---
+++ Programming Basics with C#/7. More-Excersizes/More-Exercizes-While-Loop/StreamOfLetters/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace StreamOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            string word = "";
+            string sumSecretCommand = "";
+            char currentLetter = '+';
+            string newWord = "";
+
+
+            while (command != "End")
+            {
+                command = Console.ReadLine();
+                if (command != "End")
+                {
+                    currentLetter = char.Parse(command);
+                    if ((currentLetter >= 65 && currentLetter <= 90) || (currentLetter >= 97 && currentLetter <= 122))
+                    {
+                        if (currentLetter == 'c' && !(sumSecretCommand.Contains("c")))
+                        {
+                            sumSecretCommand += char.ToString(currentLetter);
+
+                        }
+                        else if (currentLetter == 'o' && !(sumSecretCommand.Contains("o")))
+                        {
+                            sumSecretCommand += char.ToString(currentLetter);
+                        }
+                        else if (currentLetter == 'n' && !(sumSecretCommand.Contains("n")))
+                        {
+                            sumSecretCommand += char.ToString(currentLetter);
+                        }
+                        else
+                        {
+                            word += char.ToString(currentLetter);
+
+                        }
+                        if (sumSecretCommand.Contains("c") && sumSecretCommand.Contains("o") && sumSecretCommand.Contains("n"))
+                        {
+                            word += " ";
+                            newWord = word;
+                            sumSecretCommand = "";
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (command == "End")
+            {
+                Console.WriteLine(newWord);
+            }
+
+
+
+        }
+    }
+}
