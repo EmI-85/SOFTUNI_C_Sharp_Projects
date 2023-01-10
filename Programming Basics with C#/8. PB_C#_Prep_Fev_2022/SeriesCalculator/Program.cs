ac73fe5c784bb3282117aa3d4c7b1cef54214760
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-10 22:06:34 +0200

    Excersizes upload

---
+++ Programming Basics with C#/8. PB_C#_Prep_Fev_2022/SeriesCalculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.SeriesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seriesName = Console.ReadLine();
+            int seasonsNum = int.Parse(Console.ReadLine());
+            int episodesNum = int.Parse(Console.ReadLine());
+            double episodeTime = double.Parse(Console.ReadLine());
+
+            double regularTimeNeeded = seasonsNum * episodesNum * episodeTime;
+            double additionalTimeNeeded = seasonsNum * 10 + (0.20 * regularTimeNeeded);
+            double totalTime = Math.Floor(regularTimeNeeded + additionalTimeNeeded);
+
+            Console.WriteLine($"Total time needed to watch the {seriesName} series is {totalTime} minutes.");
+        }
+    }
+}
