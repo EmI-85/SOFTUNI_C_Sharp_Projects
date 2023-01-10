68dc132f9161c44d96801771565f8ce50d61d6fe
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-10 22:06:06 +0200

    Excersizes upload

---
+++ Programming Basics with C#/8. PB_C#_Prep_Fev_2022/MovieRevenue/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MovieRevenue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string movieName = Console.ReadLine();
+            int daysNum = int.Parse(Console.ReadLine());
+            int ticketsNum = int.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            int percentage = int.Parse(Console.ReadLine());
+
+            double revenuePerDay = ticketsNum * ticketPrice;
+            double revenueAllDays = revenuePerDay * daysNum;
+            double totalRevenue = revenueAllDays - (((double)percentage / 100) * revenueAllDays);
+
+            Console.WriteLine($"The profit from the movie {movieName} is {totalRevenue:f2} lv.");
+
+        }
+    }
+}
