6a80b03998041f5d0f10ffdaaba1fe6101eba1b1
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-12 10:31:27 +0200

    Excersizes upload

---
+++ Programming Basics with C#/9. Trial_Exam_9_April_2022/01.CatDiet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01.CatDiet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int percentageFats = int.Parse(Console.ReadLine());
+            int percentageProteins = int.Parse(Console.ReadLine());
+            int percentageCarbohydrates = int.Parse(Console.ReadLine());
+            int caloriesTotal = int.Parse(Console.ReadLine());
+            int percentageWater = int.Parse(Console.ReadLine());
+
+            double gramsFat = (percentageFats / 100.00 * caloriesTotal) / 9;
+            double gramsProtein = (percentageProteins / 100.00 * caloriesTotal) / 4;
+            double gramsCarbohydrate = (percentageCarbohydrates/ 100.00 * caloriesTotal) / 4;
+
+            double foodGrams = gramsFat + gramsProtein + gramsCarbohydrate;
+
+            double caloriesPerGram = caloriesTotal / foodGrams;
+
+            double realCalories = caloriesPerGram * (1 - (double)percentageWater / 100);
+
+            Console.WriteLine($"{realCalories:f4}");
+           
+            // 100/100
+
+        }
+    }
+}
