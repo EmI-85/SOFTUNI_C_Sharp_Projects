258e7cb1a9da45162e40e220d02d8df780505978
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-12 10:33:01 +0200

    Excersizes upload

---
+++ Programming Basics with C#/9. Trial_Exam_9_April_2022/06.MultiplyTable/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _06.MultiplyTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            char first = number[2];
+            char second = number[1];
+            char third = number[0];
+
+            int firstNum = int.Parse(first.ToString());
+            int secondNum = int.Parse(second.ToString());
+            int thirdNum = int.Parse(third.ToString());
+
+
+            for (int a = 1; a <= firstNum; a++)
+            {
+                for (int b = 1; b <= secondNum; b++)
+                {
+                    for (int c = 1; c <= thirdNum; c++)
+                    {
+                        double result = a * b * c;
+                        Console.WriteLine($"{a} * {b} * {c} = {result};");
+                    }
+                }
+
+            }
+
+            // 100/100
+
+            
+
+        }
+    }
+}
