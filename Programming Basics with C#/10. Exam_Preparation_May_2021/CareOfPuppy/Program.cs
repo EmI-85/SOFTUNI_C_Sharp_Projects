deda97f2481c2186aee7b644717eb00d6523db81
Emiliya Kirina <emiliya.kirina@gmail.com>
2023-01-13 18:11:19 +0200

    Excersizes upload

---
+++ Programming Basics with C#/10. Exam_Preparation_May_2021/CareOfPuppy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodAvailable = int.Parse(Console.ReadLine()) * 1000;
+            int foodEatenOverall = 0;
+
+            while(true)
+            {
+                string input = Console.ReadLine();
+
+                if(input == "Adopted")
+                {
+                    break;
+                }
+
+                int gramsEatenPerMeal = int.Parse(input);
+                
+                foodEatenOverall += gramsEatenPerMeal;
+            }
+
+            if(foodEatenOverall <= foodAvailable)
+            {
+                int foodLeftover = foodAvailable - foodEatenOverall;
+                Console.WriteLine($"Food is enough! Leftovers: {foodLeftover} grams.");
+            }
+            else
+            {
+                int foodShortage = foodEatenOverall - foodAvailable;
+                Console.WriteLine($"Food is not enough. You need {foodShortage} grams more.");
+            }
+
+        }
+    }
+}
